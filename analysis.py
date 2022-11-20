9d741cfaad5f3ac6ce1b46b18cbe8038b774b652
PamodTP <72790773+PamodTP@users.noreply.github.com>
2022-11-20 11:13:45 +0000

    Starting tokenizer

--- analysis.py
+++ analysis.py
@@ -1,10 +1,15 @@
 import nltk
 #example
 from nltk.corpus import twitter_samples
+from nltk.tokenize import TweetTokenizer
 
+#example following https://www.digitalocean.com/community/tutorials/how-to-perform-sentiment-analysis-in-python-3-using-the-natural-language-toolkit-nltk#step-6-preparing-data-for-the-model
 positive_tweets = twitter_samples.strings('positive_tweets.json')
 negative_tweets = twitter_samples.strings('negative_tweets.json')
 text = twitter_samples.strings('tweets.20150430-223406.json')
+
+tokenizer = TweetTokenizer(strip_handles=True)
+
 
 #real
 # Empty list to read list from a file
@@ -24,6 +29,9 @@ with open("data.txt", "r", encoding='utf-8') as infile:
 
 tweet_list = tweets_string.split('!NEWTWEET-DELIMITER!')
 
+tweet_tokens = tokenizer.tokenize(tweet_list)
+s0 = "This is a cooool #dummysmiley: :-) :-P <3 and some arrows < > -> <--"
+tokenizer.tokenize(s0)
 
 # display list
 print(tweet_list)
